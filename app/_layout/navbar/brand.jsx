061db3ae9625981d8d7294cc97fbd48e620cd4c3
haikal <haikal.riyadi234@gmail.com>
2026-06-03 11:57:58 +0700

    Commit

--- app/_layout/navbar/brand.jsx
+++ app/_layout/navbar/brand.jsx
@@ -11,13 +11,13 @@ export function NavbarBrand() {
 
       <div className='relative ms-2 flex overflow-hidden whitespace-nowrap transition-all duration-500 ease-in-expo group-hover:pe-8'>
         <h5 className='transition-transform duration-500 ease-in-expo group-hover:-translate-x-full'>
-          Code by
+          {/* Code by */}
         </h5>
         <h5 className='ps-1 transition-transform duration-500 ease-in-expo group-hover:-translate-x-14'>
-          Dennis
+          {/* Dennis */}
         </h5>
         <h5 className='absolute left-28 ps-1 transition-transform duration-500 ease-in-expo group-hover:-translate-x-14'>
-          Snellenberg
+          {/* Snellenberg */}
         </h5>
       </div>
     </div>
